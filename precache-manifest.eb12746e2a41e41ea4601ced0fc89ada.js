82ba04aa7ea8b05d4c6d0fd7531940faf43f5fc1
KNKRao <33758467+knkrao-naga@users.noreply.github.com>
2020-02-11 00:37:32 +0530

    Added first commit

---
+++ precache-manifest.eb12746e2a41e41ea4601ced0fc89ada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c0a9bac97aef43852c4508618d827a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8772cfaa7f507256fe6e",
+    "url": "/static/css/main.b0a142e7.chunk.css"
+  },
+  {
+    "revision": "3b7ba329f2639c4a4cc6",
+    "url": "/static/js/2.76258a39.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.76258a39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8772cfaa7f507256fe6e",
+    "url": "/static/js/main.f47c40a5.chunk.js"
+  },
+  {
+    "revision": "a76f0999e0248699d0cc",
+    "url": "/static/js/runtime-main.beb8c168.js"
+  }
+]);
